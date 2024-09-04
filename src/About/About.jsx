830ccfcf90079af4ee446df6a77ed75beaa28e96
Yashar <yashar22mo@gmail.com>
2024-09-04 10:13:44 +0330

    add about section

---
+++ src/About/About.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+	return (
+		<div className="about">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="section-header about-header">
+						<h3 className="section-title about-title">About Us</h3>
+						<p className="section-title_sub about-title_sub">Why Choose Us?</p>
+						<div className="section-line about-line"></div>
+					</div>
+					<div className="about-main">
+						<div className="about-img"></div>
+						<div className="about-content">
+							<h4 className="content-title">
+								Why Choose <span>Ronin Web Developers</span> ?
+							</h4>
+							<p className="content-caption">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Doloremque minus in similique eius voluptatibus, tempora porro
+								quibusdam? Enim fugit, eligendi magnam culpa facilis, autem quam
+								facere labore in eos dolore. Lorem ipsum dolor, sit amet
+								consectetur adipisicing elit. Repellat inventore dolores omnis,
+								accusanorrupti nam magnam nisi voluptate iure officiis assumenda
+								nemo neque distinctio? Similique, saepe laudantium.
+							</p>
+							<h5 className="content-list-title">Here is Few Features:</h5>
+							<ul className="content-list">
+								<li className="content-list-item">
+									User Interface Design (Ui)
+								</li>
+								<li className="content-list-item">
+									User Interface Design (Ux)
+								</li>
+								<li className="content-list-item">Visual Commination Design</li>
+								<li className="content-list-item">Interaction Design</li>
+								<li className="content-list-item">Supper Support</li>
+							</ul>
+							<div className="about-btns">
+								<button className="app-btn btn_blue">Send Message</button>
+								<button className="app-btn btn_white">Get Resumes</button>
+							</div>
+						</div>
+						{/* <div className="row">
+							<div className="col-lg-6">
+							</div>
+							<div className="col-lg-6">
+							</div>
+						</div> */}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header/Header";
-import Home from './Home/Home'
+import Home from "./Home/Home";
+import About from "./About/About";
 
 export default function App() {
 	return (
@@ -8,6 +9,7 @@ export default function App() {
 			<Header />
 			<main className="main">
 				<Home />
+				<About />
 			</main>
 		</div>
 	);
