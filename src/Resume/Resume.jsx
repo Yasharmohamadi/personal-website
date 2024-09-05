8f9c00dbd43d791a8a0cc0a8130c30a2f8f5b2a6
Yashar <yashar22mo@gmail.com>
2024-09-05 08:39:27 +0330

    add resume content script

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -3,11 +3,20 @@ import "./Resume.css";
 
 export default function Resume() {
 	const menuItemHandler = (event) => {
+		// menu logic
 		document
 			.querySelector(".menu__item--active")
 			.classList.remove("menu__item--active");
 
 		event.target.classList.add("menu__item--active");
+
+		// content logic
+		document
+			.querySelector(".resume-content--show")
+			.classList.remove("resume-content--show");
+
+		let itemContentId = event.target.getAttribute("item-content-id");
+		document.querySelector(itemContentId).classList.add("resume-content--show");
 	};
 	return (
 		<div className="resume">
@@ -22,6 +31,7 @@ export default function Resume() {
 						<div className="resume-menu h-full">
 							<div
 								className="menu__item menu__item--active"
+								item-content-id="#education"
 								onClick={(event) => menuItemHandler(event)}
 							>
 								<img
@@ -34,6 +44,7 @@ export default function Resume() {
 							</div>
 							<div
 								className="menu__item"
+								item-content-id="#seo-skill"
 								onClick={(event) => menuItemHandler(event)}
 							>
 								<img
@@ -46,6 +57,7 @@ export default function Resume() {
 							</div>
 							<div
 								className="menu__item"
+								item-content-id="#designer-skill"
 								onClick={(event) => menuItemHandler(event)}
 							>
 								<img
@@ -58,6 +70,7 @@ export default function Resume() {
 							</div>
 							<div
 								className="menu__item"
+								item-content-id="#work-history"
 								onClick={(event) => menuItemHandler(event)}
 							>
 								<img
@@ -70,6 +83,7 @@ export default function Resume() {
 							</div>
 							<div
 								className="menu__item"
+								item-content-id="#programming"
 								onClick={(event) => menuItemHandler(event)}
 							>
 								<img
@@ -82,48 +96,160 @@ export default function Resume() {
 							</div>
 						</div>
 					</div>
-					<div className="col-8 resume-content">
-						<div className="content-box">
-							<div className="content-header">
-								<h4 className="content-header__title">Pardis University</h4>
-								<span className="content-header__label">2018-2024</span>
+					<div className="col-8 content-wrapper">
+						<div className="resume-content resume-content--show" id="education">
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">education</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
 							</div>
-							<h5 className="content-caption__title">
-								Bsc in Computer Engineer
-							</h5>
-							<p className="content-caption__text">
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
-								beatae harum facere debitis. Nisi
-								beatae harum facere debitis.
-							</p>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">education</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">education</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
 						</div>
-						<div className="content-box">
-							<div className="content-header">
-								<h4 className="content-header__title">Pardis University</h4>
-								<span className="content-header__label">2018-2024</span>
+						<div className="resume-content" id="seo-skill">
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">seo-skill</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
 							</div>
-							<h5 className="content-caption__title">
-								Bsc in Computer Engineer
-							</h5>
-							<p className="content-caption__text">
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
-								beatae harum facere debitis. Nisi
-								beatae harum facere debitis.
-							</p>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">seo-skill</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
 						</div>
-						<div className="content-box">
-							<div className="content-header">
-								<h4 className="content-header__title">Pardis University</h4>
-								<span className="content-header__label">2018-2024</span>
+						<div className="resume-content" id="designer-skill">
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">designer-skill</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
 							</div>
-							<h5 className="content-caption__title">
-								Bsc in Computer Engineer
-							</h5>
-							<p className="content-caption__text">
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
-								beatae harum facere debitis. Nisi
-								beatae harum facere debitis.
-							</p>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">designer-skill</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
+						</div>
+						<div className="resume-content" id="work-history">
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">work-history</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
+						</div>
+						<div className="resume-content" id="programming">
+			
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">programming</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">programming</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
+							<div className="content-box">
+								<div className="content-header">
+									<h4 className="content-header__title">programming</h4>
+									<span className="content-header__label">2018-2024</span>
+								</div>
+								<h5 className="content-caption__title">
+									Bsc in Computer Engineer
+								</h5>
+								<p className="content-caption__text">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
+									beatae harum facere debitis. Nisi beatae harum facere debitis.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
